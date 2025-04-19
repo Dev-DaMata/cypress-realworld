18860d18ca6b8bc3f490eac382c87da3a32d7e01
Guilherme da Mata <gcordeiro773@gmail.com>
2025-04-19 18:55:49 -0300

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
+  viewportWidth: 1366,
+  viewportHeight: 768,
+  // projectId: 'sfjguw',
+  reporterOptions: {
+    reportDir: 'cypress/report/mochawesome-report',
+    overwrite: true,
+    reporter: 'mochawesome',
+    chromeWebSecurity: true,
+    html: true,
+    json: false,
+    timestamp: 'mmddyyyy_HHMMss',
+  },
+  env: {
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    //baseUrl: 'http://localhost:3001/',
+    specPattern: 'cypress/integration/**/*.{js,jsx,ts,tsx}',
+  },
+});
